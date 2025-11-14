ed5157d58c120f7b35dbfb931839f6b5172a3fcd
ErikMic <erikmic@hotmail.de>
2025-11-14 16:09:34 +0100

    added shopping basket

--- src/app.jsx
+++ src/app.jsx
@@ -1,6 +1,7 @@
 import Navbar from "./components/navbar";
 import Product from "./components/product";
 import React, { Component } from "react";
+import ShoppingBasket from "./components/shopping-basket";
 
 class App extends Component {
     state = {}
@@ -8,9 +9,12 @@ class App extends Component {
         return (
             <React.Fragment>
                 <Navbar />
-                <div className="product-container">
-                    <Product image="tomatoes" title="Tomaten" description="Frische und saftige Tomaten." />
-                    <Product image="cucumbers" title="Gurken" description="Knackige und kühle Gurken." />
+                <div className="main-container">
+                    <div className="product-container">
+                        <Product image="tomatoes" title="Tomaten" description="Frische und saftige Tomaten." />
+                        <Product image="cucumbers" title="Gurken" description="Knackige und kühle Gurken." />
+                    </div>
+                    <ShoppingBasket />
                 </div>
             </React.Fragment>
         );
---
+++ src/components/shopping-basket.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class ShoppingBasket extends Component {
+    state = {  } 
+    render() { 
+        return (
+            <div className="shopping-basket">
+                <h2>Warenkorb</h2>
+            </div>
+        );
+    }
+}
+ 
+export default ShoppingBasket;
